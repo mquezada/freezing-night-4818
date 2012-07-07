6a2dde0f3950957367cd1187704553f1ff291e91
Camilo Garrido <hope@NagatoYuki.(none)>
2012-07-07 18:42:02 -0400

    integracion fb perro

--- grs/gift/views.py
+++ grs/gift/views.py
@@ -1,17 +1,20 @@
 # Create your views here.
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
 from fb.fb import get_likes
+from django.shortcuts import render_to_response, redirect
+from django.http import HttpResponse
+from django.template import RequestContext
+from social_auth.models import *
 
 
 def index(request):
 	return HttpResponse("buena cabros")
 	#return render_to_response("index.kindle",{"variable":"hola"})
 
-
-def fb(request):
-	return HttpResponse(get_likes())
-
 def templates(request):
-	return render_to_response("index.kindle",{"variable":"hola"})
+	return render_to_response("index.kindle",{"variable":"hola"}, context_instance=RequestContext(request))
 
+def logged(request):
+	user = request.user
+	access_token = UserSocialAuth.objects.get(user_id=user.id).extra_data['access_token']
+
+	return render_to_response("logged.kindle",{"access_token":access_token, "username":user.username}, context_instance=RequestContext(request))
