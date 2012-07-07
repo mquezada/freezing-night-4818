f3ae913cd2a17b1f88962c2e8d2f1e42a72afa33
Camilo Garrido <hope@NagatoYuki.(none)>
2012-07-07 18:49:13 -0400

    integracion fb perro

--- grs/gift/views.py
+++ grs/gift/views.py
@@ -18,3 +18,6 @@ def logged(request):
 	access_token = UserSocialAuth.objects.get(user_id=user.id).extra_data['access_token']
 
 	return render_to_response("logged.kindle",{"access_token":access_token, "username":user.username}, context_instance=RequestContext(request))
+
+def fb(request):
+	pass
