f20f8ec55a81f66b44e4d70b2efca8907136e3ec
Camilo Garrido <hope@NagatoYuki.(none)>
2012-07-07 13:39:02 -0400

    a

--- grs/gift/views.py
+++ grs/gift/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request):
-	
-	return render_to_response("index.kindle",{"variable":"hola"})
+	return HttpResponse("buena cabros")
+	#return render_to_response("index.kindle",{"variable":"hola"})
