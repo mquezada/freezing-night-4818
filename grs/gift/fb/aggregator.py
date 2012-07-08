0be21ae2d3473787f85a0a6b1a8fb996428bbc4f
Mauricio Quezada <waxkun@gmail.com>
2012-07-08 17:36:25 -0400

    asdasd

--- grs/gift/fb/aggregator.py
+++ grs/gift/fb/aggregator.py
@@ -3,6 +3,7 @@ from paris import paris
 from ebay import ebay2
 from feriamix import feriamix
 from books.buscalibros import buscalibros
+from music.mercadolibre import mercadolibre
 from games.zmart import zmart
 
 class GiftSource(object):
@@ -22,16 +23,19 @@ def aggregate(category, terms):
 			recommendations[category] = []
 
 		if category == 'movies':
-			recommendations['movies'].extend(ebay2(term, 'movies'))
+			recommendations['movies'].extend(ebay2(term, 'movies'))			
+			recommendations['movies'].extend(mercadolibre(term, 'peliculas'))
 
 		elif category == 'music':
 			recommendations['music'].extend(ebay2(term, 'music'))
+			recommendations['music'].extend(mercadolibre(term, 'musica'))
 
 		elif category == 'books':
 			recommendations['books'].extend(buscalibros(term))
 
 		elif category == 'games':
 			recommendations['games'].extend(zmart(term))
+			recommendations['games'].extend(mercadolibre(term, 'juegos'))
 	
 	
 	return recommendations
