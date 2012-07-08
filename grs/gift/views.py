3664a5c51930f5dece853c3c4f988fd2a4fe4d51
Camilo Palma <cpalmapradena@gmail.com>
2012-07-08 00:19:28 -0400

    Foto en navbar

--- grs/gift/views.py
+++ grs/gift/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from social_auth.models import *
 from fb.fb import *
+import facebook
 
 def index(request):
 	params = {"user": None}
@@ -38,6 +39,8 @@ def fb(request):
 def logged(request):
 	request.session["user"] = request.user
 	request.session["access_token"] = UserSocialAuth.objects.get(user_id=request.user.id).extra_data['access_token']
+	graph = facebook.GraphAPI(request.session["access_token"])
+	request.session["picture"] = graph.get_connections(request.user.username, "picture")["url"]
 	return redirect("/friends")
 
 	
@@ -46,13 +49,15 @@ def friends(request):
 		return redirect("/")
 
 	user = request.session["user"]
+	picture = request.session["picture"]
+	print picture
 	access_token = request.session["access_token"]
 	
 
 	#print friend_likes(access_token, user)
 	friends = get_friends(access_token, user)
 	friends = friends[:10]
-	return render_to_response("friends.html", {"user":user, "friends":friends}, context_instance=RequestContext(request))
+	return render_to_response("friends.html", {"user":user, "friends":friends, "picture": picture}, context_instance=RequestContext(request))
 
 	#return render_to_response("logged.kindle",{"access_token":access_token, "username":user.username}, context_instance=RequestContext(request))
 
