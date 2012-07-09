c66ae9f2c6b795a2f8a764f67b49fca7cdef0e0c
Mauricio Quezada <waxkun@gmail.com>
2012-07-09 16:25:53 -0400

    asdf

--- grs/gift/views.py
+++ grs/gift/views.py
@@ -41,6 +41,8 @@ def fb(request):
 def logged(request):
 	request.session["user"] = request.user
 	m_user = dir(request.user)
+	a = dir(m_user.social_auth)
+	b = m_user.id
 	request.session["access_token"] = UserSocialAuth.objects.get(user_id=request.user.id).extra_data['access_token']
 	graph = facebook.GraphAPI(request.session["access_token"])
 	request.session["picture"] = graph.get_connections(request.user.username, "picture")['url']
